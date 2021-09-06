4e4b9f49ff264d4ae5abdbc9a27f9fa0f65f4f94
mariyavarenik <varenik.mariya@gmail.com>
2021-09-06 11:14:59 +0300

    add python selenium simple test

--- python/main.py
+++ python/main.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(executable_path=r'/usr/local/bin/chromedriver')
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+driver = webdriver.Chrome(executable_path=r'./chromedriver')
+
+driver.get("https://zxcodes.github.io/Calculator/")
+driver.find_element_by_id('one').click()
+driver.find_element_by_id('zero').click()
+driver.find_element_by_id('one').click()
+driver.find_element_by_xpath("//input[@value='+']").click()
+driver.find_element_by_id('two').click()
+driver.find_element_by_id('nine').click()
+driver.find_element_by_xpath("//input[@value='=']").click()
+assert driver.find_element_by_id('result').get_attribute('value') == '130'
+
 driver.close()
